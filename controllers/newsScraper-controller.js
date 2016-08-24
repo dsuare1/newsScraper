f64b161eb6c2775754f8c5faf6d8b5d3b8f7b657
dsuare1 <suarez.derrick@gmail.com>
2016-08-24 18:12:15 -0500

    scraping; data stored in hbsObj and rendering

--- controllers/newsScraper-controller.js
+++ controllers/newsScraper-controller.js
@@ -1,8 +1,42 @@
+var cheerio = require('cheerio');
 var express = require('express');
+var mongoose = require('mongoose');
+var request = require('request');
 var router = express.Router();
 
+var Note = require('../models/Note.js');
+var Article = require('../models/Article.js');
+
 router.get('/', function(req, res) {
-	res.send('home route loaded');
+    request('http://www.recode.net/', function(error, response, html) {
+        var $ = cheerio.load(html);
+       	var resArray = [];
+        $('.c-entry-box__title').each(function(i, element) {
+        	var data = {};
+            data.title = $(this).children('a').text();
+            data.link = $(this).children('a').attr('href');
+            // console.log(data);
+
+            resArray.push(data);
+
+            var entry = new Article (data);
+
+            entry.save(function(err, doc) {
+            	if (err) {
+            		console.log(err);
+            	} else {
+            		// console.log(doc);
+            	}
+            })
+        });
+        // console.log(resArray);
+        var hbsObj = { articles: resArray };
+        console.log(hbsObj);
+        // console.log(hbsObj);
+        // var hbsObj = { articles: data };
+        // console.log(hbsObj);
+        res.render('index', hbsObj);
+    });
 });
 
 console.log('Controller loaded --> Centralized controller (newsScraper-controller.js');
--- server.js
+++ server.js
@@ -3,35 +3,73 @@ var bodyParser = require('body-parser');
 var cheerio = require('cheerio');
 var express = require('express');
 var exphbs = require('express-handlebars');
+var logger = require('morgan');
 var mongoose = require('mongoose');
 var request = require('request');
 
+// /-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-
 // set up Express app functionality
 var app = express();
+// /-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-
 
+
+// /-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-
+// set up logger to log activity
+app.use(logger('dev'));
+
+// /-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-
 // set up Express to use static files (css, img, etc.) from the '/public' directory
 app.use(express.static(process.cwd() + '/public'));
+// /-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-
 
+
+// /-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-
+// set Express to use body-parser for easy access to request data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
-
-// parse incoming responses into body
 app.use(bodyParser.urlencoded({
 	extended: false
 }));
+// /-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-
 
+
+// /-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-
 // set up the view engine to be Handlebars
 app.engine('handlebars', exphbs({
 	defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
+// /-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-
 
+
+// /-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-
 // route to the central controller
 var routes = require('./controllers/newsScraper-controller.js');
 app.use('/', routes);
+// /-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-
 
+
+// /-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-
+// connect to MongoDB
+mongoose.connect('mongodb://localhost/newsScraper');
+var db = mongoose.connection;
+
+// show any mongoose errors
+db.on('error', function(err) {
+  console.log('Mongoose error: ', err);
+});
+
+// once logged in to the db through mongoose, log a success message
+db.once('open', function() {
+  console.log('Successfully connected to Mongoose!');
+});
+// /-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-
+
+
+// /-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-
+// set up the Express app to listen on port 3000
 app.listen(process.env.PORT || 3000, function() {
 	if (process.env.PORT == undefined) {
 		console.log('server listening on port 3000');
@@ -39,3 +77,4 @@ app.listen(process.env.PORT || 3000, function() {
     	console.log('server listening on port: ' + process.env.PORT);
     }
 });
+// /-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-
